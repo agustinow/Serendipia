730f59b754a36a5af2889c09e0924e9d64f994e7
Agustin <agustinodella@gmail.com>
2022-06-05 23:32:08 -0300

    First Commit

---
+++ app/src/main/java/com/odella/serendipia/service/FirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.odella.serendipia.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FirebaseMessagingService: Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
